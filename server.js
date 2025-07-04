368cccefe946c7e029056ad13661fb44d777132c
ALNovo21 <aleicht@novomatic.com>
2025-07-04 07:11:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/ping', (req, res) => {
 
 const pool = new Pool({
   user: 'admin',                  
-  host: '178.190.56.99',             
+  host: '178.190.76.114',             
   database: 'bestell_db',      
   password: 'workwork!',         
   port: 5432                     
